909de8248ae5fc2d6d408b67dd7ebd6511be8ffb
Mathcegep <2135850@cegeplimoilou.ca>
2023-09-16 21:41:14 -0400

    debut route

--- tp1appweb/src/App.js
+++ tp1appweb/src/App.js
@@ -3,22 +3,40 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+            <BrowserRouter>
+                <div style={{
+                    display: "flex",
+                    background: 'black',
+                    padding: '5px 0 5px 5px',
+                    fontSize: '20px'
+                }}>
+                    <div style={{ margin: '10px' }}>
+                        <NavLink to="/" style={({ isActive }) => ({ 
+                            color: isActive ? 'greenyellow' : 'white' })}>
+                            Home
+                        </NavLink>
+                    </div>
+                    <div style={{ margin: '10px' }}>
+                        <NavLink to="/about" style={({ isActive }) => ({ 
+                            color: isActive ? 'greenyellow' : 'white' })}>
+                            About
+                        </NavLink>
+                    </div>
+                    <div style={{ margin: '10px' }}>
+                        <NavLink to="/contact" style={({ isActive }) => ({ 
+                            color: isActive ? 'greenyellow' : 'white' })}>
+                            Contact
+                        </NavLink>
+                    </div>
+                </div>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/about" element={<About />} />
+                    <Route exact path="/contact" element={<Contact />} />
+                </Routes>
+            </BrowserRouter>
+        </>
   );
 }
 
---
+++ tp1appweb/src/Components/Fibonacie.jsx
@@ -0,0 +1,5 @@
+function Fibonacie() {
+
+}
+
+export default Fibonacie;
