1a0f70e6a57bf0fe21adfe32c0a8af21af9a6a4f
Mathcegep <144719358+Mathcegep@users.noreply.github.com>
2023-09-22 11:05:35 -0400

    todo fibo

--- tp1appweb/src/Components/Fibonacci.jsx
+++ tp1appweb/src/Components/Fibonacci.jsx
@@ -2,18 +2,42 @@ import { Component } from "react";
 
 class Fibonacci extends Component {
 
-    //worker = new Worker("worker.js");
+    state = {
+        limit: 0,
+        result: 0
+    }
 
-    handleClick = (e) => {
-        this.worker.postMessage()
+    changeState = (st) => {
+        this.setState({limit: st})
+    }
+
+    showFinalRes = (res) => {
+        this.setState({result: res})
+    }
+
+    fibonaccie = () => {
+        const fib = [0, 1];
+        var i = 2;
+        this.showFinalRes(this.fibonacciCalc(this.state.limit, fib, i));
+    }
+    
+    fibonacciCalc(limit, fib, i) {
+        if(i < limit) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+            i = i + 1;
+            this.fibonacciCalc(limit, fib, i);
+        } else {
+
+        }
     }
 
     render() {
         return (
             <div>
                 <h1>Fibonacci</h1>
-                <textarea></textarea>
-                <button onClick={this.handleClick} >Commencer</button>
+                <textarea onChange={() => this.changeState()}></textarea>
+                <button  onClick={() => this.fibonaccie()}>Commencer</button>
+                <p>{this.state.result}</p>
             </div>
         );
     }
